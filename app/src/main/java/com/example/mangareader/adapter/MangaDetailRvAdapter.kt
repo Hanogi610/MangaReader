b7cf5f776cefc98d8bb27044f7e0c45ef5e280f9
hp6thoct <148302503+hp6thoct@users.noreply.github.com>
2024-05-05 11:01:44 +0700

    add dummy push notification

--- app/src/main/java/com/example/mangareader/adapter/MangaDetailRvAdapter.kt
+++ app/src/main/java/com/example/mangareader/adapter/MangaDetailRvAdapter.kt
@@ -1,6 +1,12 @@
 package com.example.mangareader.adapter
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mangareader.R
 import com.example.mangareader.model.Chapter
 import com.example.mangareader.model.MangaDetail
+import java.util.Date
 
 class MangaDetailRvAdapter(private val chapters: MutableList<Chapter>, private val onItemClick : (Chapter, Int) -> Unit) : RecyclerView.Adapter<MangaDetailRvAdapter.RvChapterItemViewHolder>() {
 
@@ -52,7 +59,49 @@ class MangaDetailRvAdapter(private val chapters: MutableList<Chapter>, private v
                 newChapterCount++
             }
         }
-        Toast.makeText(context, "$newChapterCount new chapters: $newChapters", Toast.LENGTH_SHORT).show()
+        var string = ""
+        if(newChapterCount > 0){
+            string = "New chapters: " + newChapters.substring(0, newChapters.length - 2)
+            createNotificationChannel(context)
+            sendNotification(context, string)
+        }else{
+            string = "No new chapters"
+            createNotificationChannel(context)
+            sendNotification(context, string)
+        }
         notifyDataSetChanged()
+    }
+    private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "MangaReader Check New Chapters Channel"
+            val descriptionText = "Channel for Checking New Chapters notifications"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("MangaReader", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    private fun sendNotification(context: Context, s : String){
+        Log.d("Notification", "Notification sent")
+        val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.manga_icon)
+
+        val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification.Builder(context, "MangaReader")
+        } else {
+            Notification.Builder(context)
+        }
+        val builtNotification = notification.setContentTitle("New chapters check")
+            .setContentText(s)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setLargeIcon(bitmap)
+            .build()
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(getNotificationId(), builtNotification)
+    }
+    private fun getNotificationId(): Int{
+        return Date().time.toInt()
     }
 }
